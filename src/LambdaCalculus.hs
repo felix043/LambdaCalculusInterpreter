03d7f862ed54ef2ed7a0cc51908669975c0075d1
felix043 <tranfelix99@gmail.com>
2022-04-12 16:55:42 +0200

    some progress

--- src/LambdaCalculus.hs
+++ src/LambdaCalculus.hs
@@ -1,7 +1,36 @@
 module LambdaCalculus where
+import Data.List ( nub )
 
-type Id = String 
+type Id = String
 
 data Term = Var Id      -- Variables
     | Abs Id Term       -- Abstractions
     | App Term Term     -- Applications
+
+
+freeVars :: Term -> [Id]
+freeVars (Var id) = [id]
+freeVars (Abs id term) = nub (id : freeVars term)
+freeVars (App lterm rterm) = freeVars lterm ++ freeVars rterm
+
+
+-- 2. App (substitute (x, tx) (Var id)) (substitute (x, tx) term)
+
+substitute :: (Id, Term) -> Term -> Term
+substitute (x, tx) (Var a) | x == a = tx 
+                           | otherwise = Var a
+substitute (x, tx) (Abs id term) | x == id = Abs id term
+                                 | x /= id = undefined -- todo
+                                 | otherwise = Abs id term
+
+substitute (x, tx) (App lterm rterm) = App (substitute (x, tx) lterm) (substitute (x, tx) rterm)
+
+-- 3. Implement a function isBetaRedex t which returns True if the top level of the term t is a beta redex.
+
+isBetaRedex :: Term -> Bool
+isBetaRedex t = undefined 
+
+-- 4. Use substitute to implement a function betaReduce t that applies a beta reduction to top level of the term t.
+
+betaReduce :: Term -> Term
+betaReduce t = undefined
