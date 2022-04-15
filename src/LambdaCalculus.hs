9264ffb5dfec1c7218857f22114d0f97d1c1979d
felix043 <tranfelix99@gmail.com>
2022-04-15 12:00:21 +0200

    added exercise description and finished substitute

--- src/LambdaCalculus.hs
+++ src/LambdaCalculus.hs
@@ -1,5 +1,5 @@
 module LambdaCalculus where
-import Data.List ( nub )
+import Data.List (nub, (\\))
 
 type Id = String
 
@@ -7,28 +7,34 @@ data Term = Var Id      -- Variables
     | Abs Id Term       -- Abstractions
     | App Term Term     -- Applications
 
+-- 1. Implement a function freeVars t that returns a set of all free variables within a lambda term t. You may want to represent sets as lists without duplicates. 
+-- Appendix A.2 of the lecture notes contains recursive definitions of non-freeness that you may find useful.
 
 freeVars :: Term -> [Id]
 freeVars (Var id) = [id]
-freeVars (Abs id term) = nub (id : freeVars term)
+freeVars (Abs id term) = nub (freeVars term) \\ [id]
 freeVars (App lterm rterm) = freeVars lterm ++ freeVars rterm
 
-
--- 2. App (substitute (x, tx) (Var id)) (substitute (x, tx) term)
+-- 2. substitute (x,tx) t
+-- Implement a function substitute (x,tx) t that replaces all free occurrences of the variable x within the term t with the term tx. 
+-- Take care to avoid capturing substitutions (you will have to do some alpha renaming with fresh variables to avoid this).
+-- Appendix A.2 of the lecture notes contains a recursive definition of substitution that you may find useful. 
+-- In case you find the task of avoiding variable capture too challenging, skip this and only use terms with unique bound and free variable names.
 
 substitute :: (Id, Term) -> Term -> Term
 substitute (x, tx) (Var a) | x == a = tx 
                            | otherwise = Var a
-substitute (x, tx) (Abs id term) | x == id = Abs id term
-                                 | x /= id = undefined -- todo
+substitute (x, tx) (Abs id term) | x == id = Abs id tx
+                                 | x `elem ` freeVars term = substitute (x, tx) term
                                  | otherwise = Abs id term
-
 substitute (x, tx) (App lterm rterm) = App (substitute (x, tx) lterm) (substitute (x, tx) rterm)
 
 -- 3. Implement a function isBetaRedex t which returns True if the top level of the term t is a beta redex.
 
 isBetaRedex :: Term -> Bool
-isBetaRedex t = undefined 
+isBetaRedex (Var a) = False 
+isBetaRedex (Abs id term) = undefined
+isBetaRedex (App lterm rterm) = isBetaRedex lterm || isBetaRedex rterm 
 
 -- 4. Use substitute to implement a function betaReduce t that applies a beta reduction to top level of the term t.
 
